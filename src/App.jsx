18c2d08f9657316b3c96f2cdd8c710a59b4a50f7
LuciaPinzon <lusianna.pinzon@gmail.com>
2023-03-10 20:40:23 -0500

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,62 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Card from "./Components/Card";
+import "./App.css";
+
 
 function App() {
-    return (
+  const [genero, setgenero] = useState("");
+  const [artista, setartista] = useState("");
+  const [showCard, setshowCard] = useState(false);
+  const [isError, setError] = useState(false);
+
+  const isValidGeneroLength = genero.length > 2;
+  console.log(isValidGeneroLength);
+
+  const GeneroIniciaWithEspacioBlanco = genero.startsWith(" ");
+  console.log(GeneroIniciaWithEspacioBlanco);
+
+  const isValidGenero = isValidGeneroLength && !GeneroIniciaWithEspacioBlanco;
+  console.log(isValidGenero);
+
+  const isValidArtista = artista.length > 5
+
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    if(isValidArtista && isValidGenero){
+      setshowCard(true);
+      setError("");
+    }else {
+      setError("Por favor chequea que la información sea correcta");
+    }
+  };
+
+  const ongeneroChange = (e) => {
+    setgenero(e.target.value);
+  };
+
+  const onartistaChange = (e) => {
+    setartista(e.target.value);
+  };
+
+  console.log(genero, artista);
+
+  return (
     <div className="App">
+      <h1>Tu musica preferida es</h1>
+      <form onSubmit={handlesubmit}>
+        <div>
+          <input placeholder="Genero ..." type="text" value={genero} onChange={ongeneroChange} />
+        </div>
+        <div>
+          <input placeholder='Artista ...' type="text" value={artista} onChange={onartistaChange}/>
+        </div>
+        <button type="submit">Enviar</button>
+        <div> {isError}</div>
+      </form>
+      {showCard ? <Card genero={genero} artista={artista} /> : null}
     </div>
-  )
+  );
 }
 
 export default App
---
+++ src/Components/Card.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const Card = ({genero , artista}) => {
+  return (
+    <div> Hola sebemos que te gusta escuchar el {genero} y tu artista favorito es {artista}</div>
+  );
+};
+
+export default Card
