da8d9f98886159be584c8a88c6862cb8ff1b1f0f
LuciaPinzon <lusianna.pinzon@gmail.com>
2023-03-10 20:44:02 -0500

    cambios

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Card = ({genero , artista}) => {
   return (
-    <div> Hola sebemos que te gusta escuchar el {genero} y tu artista favorito es {artista}</div>
+    <div> Hola sebemos que te gusta escuchar musica {genero} y tu artista favorito es {artista}</div>
   );
 };
 
